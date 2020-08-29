d7e3d5987410355d79b35efd55090d5bfd3aad2e
jamg <alexander.menjura.1@gmail.com>
2020-08-29 12:52:37 -0500

    tarea sabado 29 de agosto 2020

--- 08_27_2020/Calculadora/assets/js/scripts.js
+++ 08_27_2020/Calculadora/assets/js/scripts.js
@@ -1,1 +1,30 @@
-const suma =
+const suma = document.getElementById("sum");
+const resta = document.getElementById("res");
+const multiplicacion = document.getElementById("mul");
+const division = document.getElementById("div");
+const resultado = document.getElementById("resultado");
+
+suma.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    sumar = num1 + num2;
+    resultado.innerHTML = "<strong>" + sumar + "</strong>";
+});
+suma.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    resta = num1 - num2;
+    resultado.innerHTML = "<strong>" + resta + "</strong>";
+});
+suma.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    multiplicacion = num1 * num2;
+    resultado.innerHTML = "<strong>" + multiplicacion + "</strong>";
+});
+suma.addEventListener("click", () => {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    division = num1 / num2;
+    resultado.innerHTML = "<strong>" + division + "</strong>";
+});
